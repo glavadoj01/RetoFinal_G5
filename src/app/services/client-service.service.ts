50cd51d54ee059f9edd049105a40ce6b012385ef
Carlos <carlosbermudezmancebo@gmail.com>
2025-06-20 11:01:55 +0200

    Servicio clientes

--- src/app/services/client-service.service.ts
+++ src/app/services/client-service.service.ts
@@ -1,11 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Cliente {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClientServiceService {
-  private http = inject(HttpClient)
-  constructor() { }
+  private apiUrl = 'https://clients-example-api.fly.dev/api/clients';
 
+  constructor(private http: HttpClient) {}
+
+  getClientes(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.apiUrl);
+  }
 }
