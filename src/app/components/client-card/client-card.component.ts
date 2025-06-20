b55fe0f49cacf7151f4bf583d3cef1e89e2c865f
Silvia <128402554+lucicu@users.noreply.github.com>
2025-06-20 11:23:45 +0200

    html y ts client-card

--- src/app/components/client-card/client-card.component.ts
+++ src/app/components/client-card/client-card.component.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-client-card',
   templateUrl: './client-card.component.html',
-  styleUrls: ['./client-card.component.css']
 })
 export class ClientCardComponent {
   @Input() client: Client = {
