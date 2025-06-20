328b009bc60c3f5cdcacc863f4d9849d2fadc662
glavadoj01 <gonzalolj@hotmail.es>
2025-06-20 11:13:29 +0200

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-<<<<<<< HEAD
-    loadComponent: () => import('./components/home/home.component')
-  },
-  {
     path: 'client',
-
-=======
-    loadComponent: () => import('./pages/home-page/home-page.module')
-  },
-  {
-    path: 'client',
-    loadComponent: () => import('./components/client-card/client-card.component')
+    loadChildren: () => import('./pages/options.routes'),
   },
   {
     path: '',
     redirectTo: 'home',
->>>>>>> 53721c4 (More pages)
   },
 ];
