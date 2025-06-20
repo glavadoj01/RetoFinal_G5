1b1baf96a748b399b3794420e2dea9dba55fa4e4
glavadoj01 <gonzalolj@hotmail.es>
2025-06-20 10:34:45 +0200

    basic structure

---
+++ src/app/components/client-card/client-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-card',
+  imports: [],
+  templateUrl: './client-card.component.html',
+})
+export class ClientCardComponent { }
